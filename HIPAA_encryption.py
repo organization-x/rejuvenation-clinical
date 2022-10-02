5d945adf7c5e279883f75947e483313afda974ca
Victor Joulin-Batejat <victorjb2015@gmail.com>
2022-10-01 21:10:48 -0700

    Cleaned up HIPAA encryption script

--- HIPAA_encryption.py
+++ HIPAA_encryption.py
@@ -1,46 +1,65 @@
 
 from cryptography.fernet import Fernet
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-key = Fernet.generate_key() # generates a random key that can be used.
-# print (key)
-# file = open('key1', 'wb')
-# file.write(key) #saves the key to a file to be used to both encrypt and decrypt
-# file.close() I have made these as comments so ypu can see what I wrote, but
-#It will not run it again. If it runs again, then a new key will generate, and
-#I don't want to deal with that to I just made it a comment.
+#------------------------------------------------------------------------------#
+def generateKey():
+    """
+    Purpose: Generating a random encryption key and saving it to a text file
+    Parameters: None
+    Returns: None
+    """
 
-# print (key)
+    key = Fernet.generate_key() # generates a random key that can be used.
 
-# import base64
-# import os
-# from cryptography.hazmat.backends import default_backend
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    print("Unsalted key:", key)
 
-# password_given = "bestpassword12"
-# password = password_given.encode()
+    # It will not run it again. If it runs again, then a new key will generate, and
+    # I don't want to deal with that to I just made it a comment.
 
-# salt = b'\xd1\x08\x1fq\xf0\x04\x03\xd4\x10\xae&G\xebL\x17\x06'
-# kdf = PBKDF2HMAC(
-#    algorithm=hashes.SHA256(),
-#    length=32,
-#    salt=salt,
-#    iterations=100000,
-#    backend=default_backend()
-#)
-# key = base64.urlsafe_b64encode(kdf.derive(password))
-# print (key)
+    password_given = "bestpassword12"
+    password = password_given.encode()
 
+    salt = b'\xd1\x08\x1fq\xf0\x04\x03\xd4\x10\xae&G\xebL\x17\x06'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+        )
 
-file = open('key1', 'rb')
-key = file.read()
-file.close()
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    with open('.env', 'wb') as file:
+        file.write(key) # saves the key to a file to be used to both encrypt and decrypt
 
-with open('test', 'rb') as f:
-    data = f.read()
+    print("Salted key:", key)
 
-fernet = Fernet(key)
-encrypted = fernet.encrypt(data)
+#------------------------------------------------------------------------------#
+if __name__ == '__main__':
 
-with open('test.encrypted', 'wb') as f:
-    f.write(encrypted)
+    generateKey()
+    with open('.env', 'rb') as f:
+        key = f.read()
+
+    with open('test.txt', 'rb') as f:
+        data = f.read()
+
+    print("Unencrypted data:", data)
+
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(data)
+
+    print("Encrypted data:", encrypted_data)
+
+    with open('test.txt', 'wb') as f:
+        f.write(encrypted_data)
+
+    byte_decrypted = fernet.decrypt(encrypted_data)
+    string_decrypted = byte_decrypted.decode()
+    print("Byte string:", byte_decrypted)
+    print("String:", string_decrypted)
