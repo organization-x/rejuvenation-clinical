690e02a3ba88f14f3716b8f87d51d1ec1a1fc23f
Victor Joulin-Batejat <victorjb2015@gmail.com>
2022-10-04 23:53:11 -0700

    Added signup functionality

--- rejuvenation/urls.py
+++ rejuvenation/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
