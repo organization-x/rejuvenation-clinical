ddd835d0a43f69a1020ecada81c35eaa9c036cc5
Victor Joulin-Batejat <victorjb2015@gmail.com>
2022-10-05 11:00:44 -0700

    Finished base custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+def get_profile_image_filepath(self):
+    return f'profile_images/{self.pk}/{"profile_image.png"}'
+
+def get_default_profile_image():
+    return "statictesting/rejuvenation_tech_logo_words.png"
+
+class Account(AbstractBaseUser):
+
+    email                   = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username                = models.CharField(max_length=30, unique=True)
+    date_joined             = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login              = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin                = models.BooleanField(default=False)
+    is_active               = models.BooleanField(default=True)
+    is_staff                = models.BooleanField(default=False)
+    is_superuser            = models.BooleanField(default=False)
+    profile_image           = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True, default=get_default_profile_image)
+    # TO BE CHANGED TO MEDICAL DOCUMENTS
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def get_medical_document_filenames(self):
+        return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/'):]
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
