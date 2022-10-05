7b2e6ceb9e02f9b28b9cdfc908c8d4c27f7c05ac
Victor Joulin-Batejat <victorjb2015@gmail.com>
2022-10-05 15:04:43 -0700

    New batch of load static, rendering in html

--- rejuvenation/urls.py
+++ rejuvenation/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("", home_screen_view, name='home'),
 ]
 if settings.DEBUG:
-    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
