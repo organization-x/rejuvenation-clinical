22e419b8f9068fe8f50f5a5d7ea14315c5d9d5be
Victor Joulin-Batejat <victorjb2015@gmail.com>
2022-10-05 11:17:01 -0700

    Added custom account manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+# create a new user
+# create a superuser
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Users must have an email address.")
+        if not username:
+            raise ValueError("Users must have a username.")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    # LATER: CREATE A 'LAB' USER
+
 def get_profile_image_filepath(self):
     return f'profile_images/{self.pk}/{"profile_image.png"}'
 
@@ -19,6 +51,8 @@ class Account(AbstractBaseUser):
     is_superuser            = models.BooleanField(default=False)
     profile_image           = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True, default=get_default_profile_image)
     # TO BE CHANGED TO MEDICAL DOCUMENTS
+
+    objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
