fae4739207952917fbf249d848392873a5b43b73
Victor Joulin-Batejat <victorjb2015@gmail.com>
2022-10-05 14:21:00 -0700

    Collected static into static_cdn, reconfigured urls

--- rejuvenation/urls.py
+++ rejuvenation/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from pages.views import (
@@ -26,3 +28,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", home_screen_view, name='home'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
